b47b45b7d73fa276f9d845458de57dbdf087f2b0
Kongxib Vue <kongcvue@gmail.com>
2021-10-15 17:13:03 -0500

    setup complete

---
+++ server/routes/todo.router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const todoRouter = express.Router();
+
+//DB connection 
+const pool = require('../modules/pool');
+
+
+//GET ROUTE
+
+//POST 
+
+//PUT
+
+//DELETE
+
+module.exports = todoRouter;
